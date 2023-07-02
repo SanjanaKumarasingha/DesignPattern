cb34017af0522c36617b25af5b44f8e0ba9f737a
ISKumarasingha <Sanjanakumarasingha1@gmail.com>
2023-07-02 05:55:17 +0530

    Memento

--- Behavioral/1 Memento Pattern/Main.java
+++ Behavioral/1 Memento Pattern/Main.java
@@ -1,3 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class History {
+    private List<EditorState> states = new ArrayList<>();
+
+    public void push(EditorState state){
+        states.add(state);
+    }
+    
+    public EditorState pop(){
+        var lastIndex = states.size() - 1;
+        var lastState = states.get(lastIndex);
+        states.remove(lastState);
+        return lastState;
+    }  
+}
+
+class EditorState {
+    private final String content;
+
+    public EditorState(String content){
+        this.content= content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
+class Editor {
+    private String content;
+    private String preContents;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public EditorState createState(){
+        return new EditorState(content);
+    }
+
+    public void restore(EditorState state){
+        content = state.getContent();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Editor editor = new Editor();
